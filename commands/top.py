6403d9587e4c06e3cbad6efbbe91eb895ce97206
Vladimir Klipert <vklipert@worldsensing.com>
2024-01-14 20:57:19 +0100

    Made top command handler produce acceptable result

--- commands/top.py
+++ commands/top.py
@@ -9,9 +9,32 @@ from telegram.ext import BaseHandler, ContextTypes, CommandHandler
 from commands.base_command_handler import BaseCommandHandler
 
 
+def _handle_top_line(line: str) -> str:
+    time = f"Time: {line[6:14]}."
+
+    def get_uptime() -> str:
+        raw_uptime = line[15:].split(",").pop(0)
+        uptime_start = -1
+        for i, c in enumerate(raw_uptime):
+            if c.isdigit():
+                uptime_start = i
+                break
+        if uptime_start < 0:
+            return ""
+        return f"Uptime: {raw_uptime[uptime_start:]}."
+
+    uptime = get_uptime()
+    return " ".join([time, uptime]) + "\n"
+
+
+line_handlers = {0: _handle_top_line}
+
+
 async def _on_top(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    result = subprocess.Popen("top -n1 -bci", shell=True, stdout=subprocess.PIPE, encoding="utf-8")
-    text = ""
+    result = subprocess.Popen(
+        "top -n1 -bci -w 512", shell=True, stdout=subprocess.PIPE, encoding="utf-8"
+    )
+    text = "```\n"
     table = PrettyTable(max_table_width=46, min_table_width=46, align="l")
     table.set_style(MARKDOWN)
     table.left_padding_width = 0
@@ -20,17 +43,17 @@ async def _on_top(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     line_number = 0
     for line in result.stdout.readlines():
         if line_number < 6:
+            line_handler = line_handlers.get(line_number)
+            text += line_handler(line) if line_handler is not None else line
             line_number += 1
-            text += line
             continue
         if line_number == 6:
             line_number += 1
             continue
         pid, user, pr, ni, virt, res, shr, s, cpu, mem, time, *command = line.split()
-        table.add_row([pid, user, cpu, mem, textwrap.fill(" ".join(command), 20)])
+        table.add_row([pid, user, cpu, mem, textwrap.fill(" ".join(command), 19)])
         line_number += 1
-    text += "```\n" + table.get_string() + "\n```"
-    print(text)
+    text += table.get_string() + "\n```"
     await context.bot.sendMessage(
         chat_id=update.effective_chat.id, text=text, parse_mode="Markdown"
     )
